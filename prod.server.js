c37fd9dd14b784912c67d4808a3bfe2fa7e432cf
wu <2697312908@qq.com>
2017-10-27 23:38:02 +0800

    add
    添加编译打包文件

---
+++ prod.server.js
@@ -0,0 +1,58 @@
+// 根据build/dev-server.js
+var express = require('express');
+var config = require('./config/index');
+
+var port = process.env.PORT || config.build.port;
+
+var app = express();
+
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+    req.url = './index.html';
+    next();
+})
+
+app.use(router);
+
+var appData = require('./data.json');
+var seller = appData.seller;
+var goods = appData.goods;
+var ratings = appData.ratings;
+
+var apiRoutes = express.Router();
+
+apiRoutes.get('/seller', function (req, res) {
+    res.json({
+        errno: 0,
+        data: seller
+    });
+});
+
+apiRoutes.get('/goods', function (req, res) {
+    res.json({
+        errno: 0,
+        data: goods
+    });
+});
+
+apiRoutes.get('/ratings', function (req, res) {
+    res.json({
+        errno: 0,
+        data: ratings
+    });
+});
+
+app.use('/api', apiRoutes);
+
+app.use(express.static('./dist'));  // 定义static目录
+
+module.exports = app.listen(port, function (err) {
+    if (err) {
+        console.log(err)
+        return
+    }
+    console.log('Listening at http://localhost:' + port + '\n')
+})
+
+
